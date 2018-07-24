243f5e924460b70be2762f47eef4f5281966ff95
easonhuang <eason620d@gmail.com>
2018-07-24 10:19:24 +0800

    1.建立Model Emp.cs,ReturnMessage.cs 2.建立Table Emp

---
+++ CRUD/Models/Emp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class Emp
+    {
+        [Display(Name = "客戶編號")]
+        public string No { get; set; }
+        [Display(Name = "客戶名稱")]
+        public string Name { get; set; }
+        [Display(Name = "年齡")]
+        public int Age { get; set; }
+        [Display(Name = "性別")]
+        public int Sex { get; set; }
+        [Display(Name = "地址")]
+        public string Address { get; set; }
+    }
+}
---
+++ CRUD/Models/ReturnMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class ReturnMessage<T>
+    {
+        /// <summary>
+        /// 結果 0.成功 1.失敗
+        /// </summary>
+        public string Result { get; set; }
+        /// <summary>
+        /// 說明/錯誤
+        /// </summary>
+        public string Message { get; set; }
+        /// <summary>
+        /// 泛型List
+        /// </summary>
+        public T ReturnList { get; set; }
+    }
+}

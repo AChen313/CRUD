2f45c815e6a9d5e399cc7aeacae14302e0b912e0
easonhuang <eason620d@gmail.com>
2018-07-24 11:14:43 +0800

    建立三層式架構 1.引用Dapper.dll及DapperExtensions.dll 2.建立Bll商業邏輯層 3.建立Controllers 4.建立View Index.cshtml 查詢頁

---
+++ CRUD/Bll/EmpService.cs
@@ -0,0 +1,58 @@
+﻿using CRUD.Models;
+using DapperExtensions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Bll
+{
+    public class EmpService
+    {
+        public static string strConnOracle = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        ///<summary>
+        ///取得使用者資料
+        ///</summary>
+        public static ReturnMessage<Emp> GetUser(string sEmp)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(strConnOracle))
+                {
+                    var pg = new PredicateGroup { Operator = GroupOperator.And, Predicates = new List<IPredicate>() };
+                    pg.Predicates.Add(Predicates.Field<Emp>(f => f.No, Operator.Eq, sEmp));
+                    var User = conn.GetList<Emp>(pg).SingleOrDefault();
+                    if (User == null)
+                    {
+                        throw new Exception("不存在的使用者資料");
+                    }
+                    return new ReturnMessage<Emp> { Result = "1", Message = "成功", ReturnList = User };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ReturnMessage<Emp> { Result = "0", Message = ex.Message, ReturnList = null };
+            }
+        }
+
+        public static ReturnMessage<List<Emp>> GetAllEmp()
+        {
+            try
+            {
+                using (var conn = new SqlConnection(strConnOracle))
+                {
+                    var User = conn.GetList<Emp>().ToList();
+
+                    return new ReturnMessage<List<Emp>> { Result = "1", Message = "成功", ReturnList = User };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ReturnMessage<List<Emp>> { Result = "0", Message = ex.Message, ReturnList = null };
+            }
+        }
+    }
+}
---
+++ CRUD/Controllers/EmpController.cs
@@ -0,0 +1,24 @@
+﻿using CRUD.Bll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: Emp
+        public ActionResult Index()
+        {
+            var result = EmpService.GetAllEmp();
+            if (result.Result == "1")
+            {
+                var model = result.ReturnList;
+                return View(model);
+            }
+            return View();
+        }
+    }
+}
